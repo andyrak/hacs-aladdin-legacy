83d3a9ef80f7257f76b81430be27227ff52be7f8
Andy Rakauskas <andy.rak@gmail.com>
2024-04-29 09:55:16 +0000

    feat: :sparkles: Add ssid and sw ver sensors, clean up logic and model (@dataclass)

--- custom_components/aladdin_connect/model.py
+++ custom_components/aladdin_connect/model.py
@@ -2,24 +2,26 @@
 
 from __future__ import annotations
 
-from typing import TypedDict
+from dataclasses import dataclass
 
 
-class DoorDevice(TypedDict):
+@dataclass
+class DoorDevice:
     """Aladdin door device."""
 
     def __eq__(self, other):
-        """Override == operator to check device_id fields."""
+        """Override == operator to check against id fields."""
         if not isinstance(other, DoorDevice):
             return NotImplemented
 
-        return self.device_id == other.device_id and self.index == other.index
+        return self.id == other.id
 
     battery_level: int
     ble_strength: int
     device_id: str
     fault: bool
     has_battery_level: bool
+    has_ble_strength: bool
     id: str
     index: int
     link_status: str
@@ -29,4 +31,6 @@ class DoorDevice(TypedDict):
     ownership: str
     rssi: int
     serial_number: str
+    software_version: str
+    ssid: str
     status: str
